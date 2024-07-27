b7d76662626a9a1359d897fb55a606b171c28672
kozanakyel <kozanakyel@gmail.com>
2024-07-27 23:03:20 +0300

    edited ts .

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,8 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export const supabase = createClientComponentClient();
 
+
+
 // export const supabase = createClient(
 //     process.env.NEXT_PUBLIC_SUPABASE_URL!,
 //     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
