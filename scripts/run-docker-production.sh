71b9c11a5eda3aef220a4821659b6bc265aa2e8c
kozanakyel <kozanakyel@gmail.com>
2024-07-27 21:04:34 +0300

    Set up Docker and deployment

---
+++ scripts/run-docker-production.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKER_COMPOSE_FILE=docker-compose.yml
+DOCKER_SERVICE_NAME="next-supa-base"
+
+cd /home/next-supa-base
+
+docker-compose -f $DOCKER_COMPOSE_FILE pull
+docker-compose -f $DOCKER_COMPOSE_FILE stop $DOCKER_SERVICE_NAME
+docker-compose -f $DOCKER_COMPOSE_FILE build
+docker-compose -f $DOCKER_COMPOSE_FILE up -d $DOCKER_SERVICE_NAME nginx
+
+docker rmi $(docker images -f "dangling=true" -q) 2>/dev/null
