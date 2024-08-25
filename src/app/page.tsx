71bfda93b5c7503e767f97afa6355e3eb8c10dbc
kozanakyel <kozanakyel@gmail.com>
2024-08-25 12:26:58 +0300

    edited home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,39 +3,39 @@
 import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  const [selectedVideo, setSelectedVideo] = useState<string>('');
+  // const videoRef = useRef<HTMLVideoElement | null>(null);
+  // const [selectedVideo, setSelectedVideo] = useState<string>('');
 
-  useEffect(() => {
-    // Array of video file names
-    const videos = ['/fakearm.mp4', '/singleman.mp4', '/teamcollabarate.mp4'];
+  // useEffect(() => {
+  //   // Array of video file names
+  //   const videos = ['/fakearm.mp4', '/singleman.mp4', '/teamcollabarate.mp4'];
 
-    // Select a random video
-    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+  //   // Select a random video
+  //   const randomVideo = videos[Math.floor(Math.random() * videos.length)];
 
-    // Set the selected video
-    setSelectedVideo(randomVideo);
+  //   // Set the selected video
+  //   setSelectedVideo(randomVideo);
 
-    // Attach an event listener for enabling sound
-    const enableSound = () => {
-      if (videoRef.current) {
-        videoRef.current.muted = false;
-        videoRef.current.play();
-        document.removeEventListener('click', enableSound);
-      }
-    };
+  //   // Attach an event listener for enabling sound
+  //   const enableSound = () => {
+  //     if (videoRef.current) {
+  //       videoRef.current.muted = false;
+  //       videoRef.current.play();
+  //       document.removeEventListener('click', enableSound);
+  //     }
+  //   };
 
-    document.addEventListener('click', enableSound);
+  //   document.addEventListener('click', enableSound);
 
-    // Clean up event listener on component unmount
-    return () => {
-      document.removeEventListener('click', enableSound);
-    };
-  }, []);
+  //   // Clean up event listener on component unmount
+  //   return () => {
+  //     document.removeEventListener('click', enableSound);
+  //   };
+  // }, []);
 
   return (
     <div className="relative flex flex-col justify-between min-h-screen overflow-hidden">
-      {selectedVideo && (
+      {/* {selectedVideo && (
         <video
           ref={videoRef}
           autoPlay
@@ -47,12 +47,18 @@ export default function Home() {
           <source src={selectedVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      )}
+      )} */}
 
       <main className="flex-grow flex flex-col items-center justify-center text-center relative z-10 p-4">
-        <h1 className="text-6xl font-bold text-white">KZAI</h1>
-        <p className="mt-4 text-lg font-serif text-white sm:mt-6 sm:px-4">
-          Problem Solving for AI based Applications
+        <h1 className="text-6xl font-bold text-black">KZAI</h1>
+        <p className="mt-4 text-lg font-serif text-black sm:mt-6 sm:px-4">
+          For AI based Applications
+        </p>
+        <p className="mt-4 text-lg font-serif text-black sm:mt-6 sm:px-4">
+          Problem Solving
+        </p>
+        <p className="mt-4 text-lg font-serif text-black sm:mt-6 sm:px-4">
+          System Design
         </p>
       </main>
 
